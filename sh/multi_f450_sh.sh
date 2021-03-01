7f4c2ab2dc90c984cc750bb5d0d8fdfa87a0405a
chenc159 <kccchen@gmail.com>
2021-02-28 22:19:48 -0500

    20210228

--- sh/multi_f450_sh.sh
+++ sh/multi_f450_sh.sh
@@ -1,7 +1,5 @@
-# gnome-terminal --window -e 'ssh -X odroid@192.168.2.117' \
-# --tab -e 'ssh -X odroid@192.168.2.179' \
-# gnome-terminal --window -e 'bash -c "roslaunch f450 f450_test.launch uavID:=uav1 tgt_system:=1; exec bash"' \
-# --tab -e 'bash -c "sleep 2; roslaunch f450 f450_test.launch uavID:=uav2 tgt_system:=2; exec bash"' \
+gnome-terminal --window -e 'bash -c "roslaunch f450 f450_test.launch uavID:=uav1 tgt_system:=1; exec bash"' \
+--tab -e 'bash -c "sleep 2; roslaunch f450 f450_test.launch uavID:=uav2 tgt_system:=2; exec bash"' \
 # --tab -e 'bash -c "sleep 2; rosrun outdoor_gcs outdoor_gcs; exec bash"' \
 # --tab -e 'bash -c "sleep 2; roslaunch f450 f450_test.launch uavID:=uav3 tgt_system:=3; exec bash"' \
 # --tab -e 'bash -c "sleep 2; roslaunch f450 f450_test.launch uavID:=uav4 tgt_system:=4; exec bash"' \
