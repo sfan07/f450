6066e3925b9ca486b8fd02a4c46cc50ee2590e16
chenc159 <kccchen@gmail.com>
2021-04-19 23:02:38 -0400

    20210419

--- sh/sitl.sh
+++ sh/sitl.sh
@@ -1,7 +1,9 @@
-gnome-terminal --window -e 'bash -c "roslaunch px4 multi_uav_mavros_sitl.launch"' \
---tab -e 'bash -c "sleep 20; roslaunch px4_command px4_multidrone_pos_estimator_outdoor.launch uavID:=uav1; exec bash"' \
---tab -e 'bash -c "sleep 20; roslaunch px4_command px4_multidrone_pos_controller_outdoor.launch uavID:=uav1; exec bash"' \
---tab -e 'bash -c "sleep 2; rosrun outdoor_gcs outdoor_gcs"' \
+gnome-terminal --window -e 'bash -c "roslaunch f450 multi_uav_mavros_sitl.launch"' \
+--tab -e 'bash -c "sleep 10; roslaunch px4_command px4_multidrone_pos_estimator_outdoor.launch uavID:=uav1; exec bash"' \
+--tab -e 'bash -c "sleep 10; roslaunch px4_command px4_multidrone_pos_controller_outdoor.launch uavID:=uav1; exec bash"' \
+--tab -e 'bash -c "sleep 10; roslaunch px4_command px4_multidrone_pos_estimator_outdoor.launch uavID:=uav2; exec bash"' \
+--tab -e 'bash -c "sleep 10; roslaunch px4_command px4_multidrone_pos_controller_outdoor.launch uavID:=uav2; exec bash"' \
+--tab -e 'bash -c "sleep 10; rosrun outdoor_gcs outdoor_gcs"' \
 
 
 
